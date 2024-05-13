bd6594c4538cb08dcbba051ae59185e63a649410
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-13 21:49:51 +0530

    modules

--- 03-modules.js
+++ 03-modules.js
@@ -6,8 +6,14 @@ const sayHi = require('./05-utils')
 
 // Alternate exports import
 const data = require('./06-alternate-exports')
-console.log(data)
 
+// Modules - Each js file in node is a module
+// Module is encapsulated code (only share minimum)
+
+require('./07-no-export-function-call')
+/** --- This is important to note here
+ *  We imported a file in which a function was getting invoked and the function got called as we run this file.
+ *  --- */
 console.log(names)
 sayHi(names.peter)
 sayHi(names.sam)
--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-// Modules - Each js file in node is a module
-// Module is encapsulated code (only share minimum)
 
-require('./07-no-export-function-call')
-/** --- This is important to note here
- *  We imported a file in which a function was getting invoked and the function got called as we run this file.
- *  --- */
