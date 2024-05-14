fd751d8cad87563afdadd228cd53dc8b4b895d52
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-14 22:48:41 +0530

    Successfully read & write from files asynchronously

--- app.js
+++ app.js
@@ -2,10 +2,33 @@ const { readFile, writeFile } = require('fs')
 
 // Now, we will read and write to file asynchronously. And for this we'll need to provide a callback once we are done!
 
-readFile('./content/first.txt', 'utf8', (error, result) => {
+readFile('./content/first.txt', 'utf8', (error, result1) => {
   if (error) {
     return
   }
 
-  console.log(result)
+  console.log(result1)
+
+  const fileOneText = result1
+
+  readFile('./content/second.txt', 'utf8', (error, result2) => {
+    if (error) {
+      return
+    }
+
+    console.log(result2)
+    const fileTwoText = result2
+
+    writeFile(
+      './content/result-async.txt',
+      `This is my Async file, It contains:\n ${fileOneText} & \n ${fileTwoText} `,
+      (err, result3) => {
+        if (err) {
+          return
+        }
+
+        console.log(result3)
+      }
+    )
+  })
 })
