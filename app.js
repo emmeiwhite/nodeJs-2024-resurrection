4a5fc4424f14ee62374070d974b44dc8c4a72904
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-15 00:52:19 +0530

    Basic server ready ; return is must for each route

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const http = require('http')
 
 // Creating a server --- wowww ! This is awesome > Though later we use Express to create server more easily
 const server = http.createServer((req, res) => {
-  res.write('Welcome to our homepage')
+  if (req.url === '/') {
+    res.end('Welcome to the home page')
+    return
+  }
+
+  if (req.url === '/about') {
+    res.end('We are fellow coders and web technology geeks')
+    return
+  }
+
+  // If the route doesn't match
+  res.end(`<div>
+      <h1>The Page is not found</h1>
+      <a href="/">Back to the Homepage</a>
+    </div>`)
+
   res.end()
+  //   res.write('Welcome to our homepage')
+  //   res.end()
 })
 
 server.listen(5000)
