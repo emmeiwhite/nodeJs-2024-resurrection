b15a5c0c8da495f89fbc4b28b445c02b41f6c2f2
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-15 01:12:23 +0530

    Setting script to automate our tasks - nodemon in action

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-const http = require('http')
+const _ = require('lodash')
 
-// Creating a server --- wowww ! This is awesome > Though later we use Express to create server more easily
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to the home page')
-    return
-  }
-
-  if (req.url === '/about') {
-    res.end('We are fellow coders and web technology geeks')
-    return
-  }
-
-  // If the route doesn't match
-  res.end(`<div>
-      <h1>The Page is not found</h1>
-      <a href="/">Back to the Homepage</a>
-    </div>`)
-
-  res.end()
-  //   res.write('Welcome to our homepage')
-  //   res.end()
-})
-
-server.listen(5000)
+// Using Lodash to clone an object
+const object = { a: 1, b: 2 }
+const clone = _.cloneDeep(object)
+console.log(clone)
